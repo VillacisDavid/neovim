62a96735794be3a7f2504840ae357a71e9eded74
VillacisDavid <david.villacis@unmsm.edu.pe>
2023-11-24 21:08:43 -0500

    Edit

--- lua/davo/set.lua
+++ lua/davo/set.lua
@@ -9,11 +9,11 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
+vim.opt.wrap = true
 
 vim.opt.swapfile = false
 vim.opt.backup = false
-vim.opt.undodir = "undodir"
+vim.opt.undodir = "C:\\Users\\David\\AppData\\Local\\nvim\\undodir"
 vim.opt.undofile = true
 
 vim.opt.hlsearch = true
